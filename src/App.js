75bbf683549393528acc428e78fce0a8a016f07d
bimogempar <bimogempar@gmail.com>
2022-07-21 14:44:41 +0700

    add react router dom v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
