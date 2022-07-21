5d21996a7ee2a3ccdea0f7bddc2f777141400de1
bimogempar <bimogempar@gmail.com>
2022-07-21 14:35:56 +0700

    add setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-
-    </Router>
+    <h1 className="text-3xl font-bold">
+      Hello world!
+    </h1>
   );
 }
 
